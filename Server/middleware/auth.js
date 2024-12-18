53ae6ad88b1f127fbc91234a626178c7d55ccd33
Hrayr <138680803+HrayrHayrapetyan@users.noreply.github.com>
2024-12-18 21:52:35 +0400

    Update auth.js

---
+++ Server/middleware/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken"
+
+function requireAuth (req, res, next) {
+
+    console.log('inside auth');
+    
+    const token = req.cookies.token;
+    console.log('token taken ');
+    
+    if (token) {
+        jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+            if (err){
+                return res.status(401).json({message: 'Invalid or expired token'})
+            } 
+            res.user = decoded;
+          return next();
+        })
+    }
+    else {
+        res.status(401).json({message:'No token provided'})
+    }
+}
+
+export default requireAuth
