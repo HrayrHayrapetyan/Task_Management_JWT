a09b6a5072be4f64dea18e5f6a89a69ba257ec5b
Hrayr <138680803+HrayrHayrapetyan@users.noreply.github.com>
2024-12-18 21:53:26 +0400

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -5,25 +5,27 @@ import dbConnect from './config/db.js'
 import cors from 'cors'
 import authRoutes from "./routes/auth.js"
 import path from 'path'
+import cookieParser from 'cookie-parser'
 
 const app=express()
 
 const dirname = path.join(process.cwd(), '..',); // Correctly resolve the path
+app.use(cookieParser())
 
 app.use(express.static(path.join(dirname, 'Client')))
 
 app.use(express.json())
 app.use(cors())
 
+
 app.use(authRoutes)
 const port=3000
 
 dbConnect().then(()=>{
     app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
-})
-
+        console.log(`Server is running on port http://localhost:${port}`)
+        })
+        })
 
 
 
